a85eb144cb9dfd17c8a96b63819683aee2af1d54
Marvin <marvin131t@gmail.com>
2022-02-03 17:19:40 +0100

    Did someone say burrito?

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -1,10 +1,11 @@
 public class Constants {
 
 	public static Double GRAVITY_EARTH = 9.82;
-	public static Float PI = 3.14159265359F;
-	public static Float R = 0.1889F; // N2O
-	public static Float heatRatio = 1.31F; // N2O
-	public static Float standardPow = ((heatRatio + 1) / (2 * (heatRatio - 1)));
+	public static Double PI = 3.14159265359;
+	public static Double R = 0.1889; // N2O
+	public static Double heatRatio = 1.31; // N2O
+	public static Double standardPow = ((heatRatio + 1) / (2 * (heatRatio - 1)));
 	public static Boolean isInVacuum = false;
+	public static Double freeStreamPressure = 1.01325353;
 //	public static Float C12H26C15H32 =
 }
